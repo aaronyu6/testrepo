cabb8e21fab4acf1a653bfe0f27989c5e5263fb2
aaron yu <service@sghypno.com>
2020-11-15 00:43:26 +0800

    Course Comp, display course name from prop succeed

---
+++ src/course.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Course =({courseProp})=>(
+    <div>{courseProp.coursename}</div>
+);
--- test/course.test.js
+++ test/course.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Course} from '../src/course';
+
 describe('course', ()=>{
  it('first time',()=>{
      function abc(){
@@ -7,5 +11,13 @@ describe('course', ()=>{
      }
      expect(abc()).toEqual(2);
 
+ });
+ it('display course name from prop',()=>{
+    //render the component, expect container's text equal prop
+     let container=document.createElement('div');
+     let courseProp={coursename:"course name abc"};
+     let courseComp=<Course courseProp={courseProp}/>;
+     ReactDom.render(courseComp,container);
+     expect(container.textContent).toMatch('course name abc');
  });
 });
