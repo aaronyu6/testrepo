87eb54b0d88d08af36decf5f8f2c51bf49d2ad5a
aaron yu <service@sghypno.com>
2020-11-14 17:34:59 +0800

    pass jest dummy test

---
+++ test/course.test.js
@@ -0,0 +1,11 @@
+describe('course', ()=>{
+ it('first time',()=>{
+     function abc(){
+         let a=1;
+         let b=1;
+         return a+b;
+     }
+     expect(abc()).toEqual(2);
+
+ });
+});
